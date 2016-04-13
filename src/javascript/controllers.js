07a8e5313a6c5eecc702a1edd5d2687b1f2337a4
Andrew Trigg <andrewt51@hotmail.com>
2016-04-13 19:35:01 +0100

    Added gradient-faded background image on 'about' page

--- src/javascript/controllers.js
+++ src/javascript/controllers.js
@@ -1,3 +1,11 @@
 app.controller("mainCtrl", function(){
-  console.log('hello');
+  var menuVisible = false;
+  var menu = document.getElementsByClassName('navigation-bar')[0];
+  var logo = document.getElementsByClassName('logo')[0];
+  logo.addEventListener('click', handleMenu)
+  function handleMenu(){
+    menuVisible = !menuVisible;
+    menu.className = menuVisible ? menu.className + " menu-visible" : 'navigation-bar';
+  }
+  
 })
