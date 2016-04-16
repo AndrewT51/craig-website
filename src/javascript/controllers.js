86f15a65a74352417e283e14559362ace4a5ebc7
Andrew Trigg <andrewt51@hotmail.com>
2016-04-17 00:53:12 +0100

    Mobile menu rethought

--- src/javascript/controllers.js
+++ src/javascript/controllers.js
@@ -3,14 +3,23 @@ app.controller("mainCtrl", function($state, $scope){
   var menu = document.getElementsByClassName('navigation-bar')[0];
   var logo = document.getElementsByClassName('logo')[0];
   logo.addEventListener('click', handleMenu)
+  
+  onresize = function(){
+    menu.className = 'navigation-bar';
+    menuVisible = false;
+  }
+
   function handleMenu(){
     menuVisible = !menuVisible;
-    menu.className = menuVisible ? menu.className + " menu-visible" : 'navigation-bar';
+    menu.className = menuVisible ? "navigation-bar menu-visible" : 'navigation-bar';
   }
+
   $scope.changeScreen = function(page){
-    $state.go(page);
     handleMenu();
+    console.log(page)
+    $state.go(page);
   }
+
   $scope.services = data;
   
 })
