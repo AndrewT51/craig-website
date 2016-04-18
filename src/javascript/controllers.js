4a00c10d88b5a3dcdd785f0c5ac57a2f3460c2ff
Andrew Trigg <andrewt51@hotmail.com>
2016-04-19 00:17:15 +0100

    Testimonials added

--- src/javascript/controllers.js
+++ src/javascript/controllers.js
@@ -23,3 +23,20 @@ app.controller("mainCtrl", function($state, $scope){
   $scope.services = data;
   
 })
+
+.controller("testimonials", function($scope){
+  var quoteNumber = 0;
+  var testimonial = document.querySelector('.testimonial');
+  var author = document.querySelector('.author');
+  // var author = document.createElement('cite');
+  // var display = document.querySelector('.main-pic-container');
+  testimonialBox();
+  function testimonialBox(){
+    testimonial.textContent = testimonials[quoteNumber].quote;
+    author.textContent = testimonials[quoteNumber].customer
+    // display.appendChild(testimonial,author);
+    quoteNumber === 4 ? quoteNumber = 0 : quoteNumber ++;
+    
+  }
+  setInterval(testimonialBox,5000);
+})
--- website-data.js
+++ website-data.js
@@ -26,3 +26,26 @@ var data = [
     }
   
 ]
+
+var testimonials = [
+  {
+    customer: "Rob O'Brien",
+    quote: "Top bloke, amazing carpentry a with top service. If you need a carpenter Craig's your man"
+  },
+  {
+    customer: "Lisa Haines",
+    quote: "Was very happy with work and would most def use craig again."
+  },
+  {
+    customer: "Michelle M Donofrio",
+    quote: "Fantastic job turning an old counter into something special...will be using Craig Hill Carpentry again very soon."
+  },
+  {
+    customer: "Michelle Davis",
+    quote: "Thank you for our made-to-measure shoe rack, it's made a real difference to our hallway and just what I was hoping for! And super quick too - cheers!"
+  },
+  {
+    customer: "Meriel Singleton",
+    quote:"Craig has just built a couple of cupboards in our house and a raised bed for our garden - his work was excellent and Craig was really helpful and professional. We will definitely use him for any future work!"
+  }
+]
